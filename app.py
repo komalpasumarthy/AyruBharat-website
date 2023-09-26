5a4c9e212faeaa528b71762c7973985e2859ecf5
jaswanthlsvyj <91729041+jaswanthlsvyj@users.noreply.github.com>
2023-09-26 18:31:21 +0000

    added requirements

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# index page
 @app.route("/")
 def index():
-  return 'hello'
+  return render_template('index.html')
+
+# login page
+@app.route("/login")
+def login():
+  return render_template('login.html')
+
+# signup page
+@app.route("/signup")
+def signup():
+  return render_template('signup.html')
 
 
 if __name__ == '__main__':
